e4f76d595a198c5d7e6ff377558a17fa2f426765
tony.ko <31752048+tainenko@users.noreply.github.com>
2025-06-28 23:20:22 +0800

    Add database migrations and .env for Goose configuration

---
+++ migrations/20250628151326_create_task_table.sql
@@ -0,0 +1,17 @@
+-- +goose Up
+-- +goose StatementBegin
+SELECT 'up SQL query';
+CREATE TABLE IF NOT EXISTS task
+(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    status INTEGER NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+                             );
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS task
+-- +goose StatementEnd
